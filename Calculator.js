fc52fdaa0b60318585dde6db334fb96869c02a43
Yuki-275 <75024064+Yuki-275@users.noreply.github.com>
2020-11-25 18:18:31 +0530

    Add files via upload

---
+++ Calculator.js
@@ -0,0 +1,19 @@
+function add1() {
+    var a1 = Number(document.getElementById('fn').value) + Number(document.getElementById('sn').value)
+    window.alert(a1);
+}
+
+function subtract1() {
+    var a1 = Number(document.getElementById("fn").value) - Number(document.getElementById("sn").value)
+    window.alert(a1);
+}
+
+function multi1() {
+    var a1 = Number(document.getElementById("fn").value) * Number(document.getElementById("sn").value)
+    window.alert(a1);
+}
+
+function div1() {
+    var a1 = Number(document.getElementById("fn").value) / Number(document.getElementById("sn").value)
+    window.alert(a1);
+}
